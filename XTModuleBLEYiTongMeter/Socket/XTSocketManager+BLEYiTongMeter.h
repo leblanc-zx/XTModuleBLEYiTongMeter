d5406be126cc9bc84df2e7825e0cb3956fbc9f54
leblanc-zx <357106354@qq.com>
2019-04-17 17:04:12 +0800

    1.0.3

---
+++ XTModuleBLEYiTongMeter/Socket/XTSocketManager+BLEYiTongMeter.h
@@ -0,0 +1,17 @@
+//
+//  XTSocketManager+BLEYiTongMeter.h
+//  XTModuleBLEYiTongMeter
+//
+//  Created by apple on 2019/4/17.
+//  Copyright © 2019年 新天科技股份有限公司. All rights reserved.
+//
+
+#import "XTSocketManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XTSocketManager (BLEYiTongMeter)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XTModuleBLEYiTongMeter/Socket/XTSocketManager+BLEYiTongMeter.m
@@ -0,0 +1,13 @@
+//
+//  XTSocketManager+BLEYiTongMeter.m
+//  XTModuleBLEYiTongMeter
+//
+//  Created by apple on 2019/4/17.
+//  Copyright © 2019年 新天科技股份有限公司. All rights reserved.
+//
+
+#import "XTSocketManager+BLEYiTongMeter.h"
+
+@implementation XTSocketManager (BLEYiTongMeter)
+
+@end
